c649960d18851188b633afd64385aaeac3dd9a25
Jacob Lang <46350959+Jacob-Lang@users.noreply.github.com>
2021-03-30 08:17:00 +0100

    Fix clear_output bug in swiper.py by switching to widget.HTML()

--- swiper.py
+++ swiper.py
@@ -7,65 +7,55 @@ class Swiper():
     def __init__(self, df,  model):
         self.df = df
         self.model = model
+               
+        # widget buttons for each action
+        self.swipe_right = widgets.Button(description='Yes please! :D')
+        self.swipe_left = widgets.Button(description='No thanks. :|')
         
+        self.recipe_html = widgets.HTML()
+        self.buttons = widgets.HBox([self.swipe_left, self.swipe_right], layout=widgets.Layout(justify_content='center'))
+            
+        self.swipe_left.on_click(lambda _ : self.left())
+        self.swipe_right.on_click(lambda _ : self.right())
+        
+    def right(self):
+        reward = 1
+        self.model.train_step((self.action, reward))
+        self.action = self.model.call()
+        self.recipe_html.value = self.get_recipe_html(self.action)
+
+    def left(self):
+        reward = 0
+        self.model.train_step((self.action, reward))
+        self.action = self.model.call()
+        self.recipe_html.value = self.get_recipe_html(self.action)
+
     def run(self):
         
         # page header
         display(HTML("<h1 align=\"center\"> Recipe Swiper </h2>"))
         display(HTML("<center> Looking for love in a plate of food <br> <br> <br> </center>"))
-                
-        # widget buttons for each action
-        self.swipe_right = widgets.Button(description='Yes please! :D')
-        self.swipe_left = widgets.Button(description='No thanks. :|')
         
-        buttons = widgets.HBox([self.swipe_left, self.swipe_right], layout=widgets.Layout(justify_content='center'))
-        output = widgets.Output(layout=widgets.Layout(height='500px'))      
-        # display buttons
-        display(widgets.VBox([output, buttons]))
-        
+        display(self.recipe_html)
+        display(self.buttons)
+
         # first recipe
         self.action = self.model.call()
-
-        with output:
-            self.display_recipe(self.action)
-            clear_output(wait=True)
+        self.recipe_html.value = self.get_recipe_html(self.action)
     
-        
-        def right(self):
-            reward = 1
-            self.model.train_step((self.action, reward))
-            self.action = self.model.call()
-            with output:
-                self.display_recipe(self.action)
-                clear_output(wait=True)
-                
-        def left(self):
-            reward = 0
-            self.model.train_step((self.action, reward))
-            self.action = self.model.call()
-            with output:
-                self.display_recipe(self.action)
-                clear_output(wait=True)
-        
-        self.swipe_left.on_click(lambda _ : left(self))
-        self.swipe_right.on_click(lambda _ : right(self))
-        
-        def reset():
-            # add button for reset
-            pass
+    def reset():
+        # add button for reset
+        pass
             
-    def display_recipe(self, action):
+    def get_recipe_html(self, action):
         title = self.df.loc[action.numpy(), 'title']
         summary = self.df.loc[action.numpy(), 'summary']
         image_url = self.df.loc[action.numpy(), 'image_url']
         page_url = self.df.loc[action.numpy(), 'page_url']
 
-#         display(Markdown(title))
-#         display(Markdown(summary))
-#         display(Image(url=url))
-
-        display(HTML("<h2 align=\"center\"> " + title +  "</h2>"))
-        display(HTML("<center>" + summary +  "<center>"))
-        display(HTML("<p style=\"text-align:center;\"> <a href=" + page_url + "> <img src=" + image_url + " alt=\"recipe_img\" class=\"center\"> </a> </p> "))
-
+        html = "<h2 align=\"center\"> " + title +  "</h2>" \
+                + "<center>" + summary +  "<center> <br>" \
+                + "<p style=\"text-align:center;\"> <a href=" + page_url + "> <img src=" + image_url + " alt=\"recipe_img\" class=\"center\"> </a> </p> "
         
+        return html
+ 
